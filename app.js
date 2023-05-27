77c2896dd4ff5b7f19cd9f69b23540dc67e0cb86
Siicked <109355725+Siicked@users.noreply.github.com>
2023-05-27 03:33:23 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,149 @@
+
+function cargarAnimaciones(){
+    $(function(){
+        $('.chart1').easyPieChart({
+            size:160,
+            barColor:"#ff5b00",
+            scaleLength:0,
+            lineWidth:15,
+            tackColor:"#525151",
+            lineCap:"circle",
+            animate:2000,
+        });
+        $('.chart2').easyPieChart({
+            size:160,
+            barColor:"#ff5b00",
+            scaleLength:0,
+            lineWidth:15,
+            tackColor:"#525151",
+            lineCap:"circle",
+            animate:2000,
+        });
+        $('.chart3').easyPieChart({
+            size:160,
+            barColor:"#ff5b00",
+            scaleLength:0,
+            lineWidth:15,
+            tackColor:"#525151",
+            lineCap:"circle",
+            animate:2000,
+        });
+        $('.chart4').easyPieChart({
+            size:160,
+            barColor:"#ff5b00",
+            scaleLength:0,
+            lineWidth:15,
+            tackColor:"#525151",
+            lineCap:"circle",
+            animate:2000,
+        });
+        $('.chart5').easyPieChart({
+            size:160,
+            barColor:"#ff5b00",
+            scaleLength:0,
+            lineWidth:15,
+            tackColor:"#525151",
+            lineCap:"circle",
+            animate:2000,
+        });
+        $('.chart6').easyPieChart({
+            size:160,
+            barColor:"#ff5b00",
+            scaleLength:0,
+            lineWidth:15,
+            tackColor:"#525151",
+            lineCap:"circle",
+            animate:2000,
+        });
+        $('.chart7').easyPieChart({
+            size:160,
+            barColor:"#ff5b00",
+            scaleLength:0,
+            lineWidth:15,
+            tackColor:"#525151",
+            lineCap:"circle",
+            animate:2000,
+        });
+        $('.chart8').easyPieChart({
+            size:160,
+            barColor:"#ff5b00",
+            scaleLength:0,
+            lineWidth:15,
+            tackColor:"#525151",
+            lineCap:"circle",
+            animate:2000,
+        });
+
+    })
+}
+function verCategoria(cat){
+    const items = document.getElementsByClassName("item");
+    for(let i=0; i < items.length;i++){
+        items[i].style.display = "none";
+    }
+
+    const itemCat = document.getElementsByClassName(cat);
+    for(let i = 0; i<itemCat.length;i++){
+        itemCat[i].style.display = "block";
+    }
+
+    const links = document.querySelectorAll(".trabajos nav a");
+    links[0].className = "";
+    links[1].className = "";
+    links[2].className = "";
+    links[3].className = "";
+
+    const itemSeleccionado = document.getElementById(cat);
+    itemSeleccionado.className = "borde";
+}
+
+function efectoHabilidades(){
+    var skills = document.getElementById("habilidades");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("chart");
+        habilidades[0].classList.add("chart1");
+        habilidades[1].classList.add("chart2");
+        habilidades[2].classList.add("chart3");
+        habilidades[3].classList.add("chart4");
+        habilidades[4].classList.add("chart5");
+        habilidades[5].classList.add("chart6");
+        habilidades[6].classList.add("chart7");
+        habilidades[7].classList.add("chart8");
+
+        cargarAnimaciones();
+    }
+}
+
+window.onscroll = function(){
+    efectoHabilidades();
+}
+
+function responsiveMenu(){
+    let x = document.getElementById("nav");
+    if(x.className===""){
+        x.className = "responsive";
+
+        let span = document.createElement("span");
+        span.innerHTML = "X";
+        document.getElementById("nav").appendChild(span);
+
+        span.onclick = function(){
+            x.className = "";
+            span.remove();
+        }
+    }else{
+        x.className="";
+    }
+}
+
+const links = document.querySelectorAll("#nav a");
+for(i=0; i < links.length;i++){
+    links[i].onclick = function(){
+        var x = document.getElementById("nav");
+        x.className = "";
+
+        btnEliminar = document.querySelector("#nav span");
+        btnEliminar.remove();
+    }
+}
